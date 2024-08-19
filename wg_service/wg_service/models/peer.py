747db96dcb97915de34b76c4978aa0552652177b
andvarfolomeev <varfolomeev.av@icloud.com>
2024-08-20 02:43:23 +1000

    feature: allowed_ips is unique in peer model

--- wg_service/wg_service/models/peer.py
+++ wg_service/wg_service/models/peer.py
@@ -2,10 +2,8 @@ from ipaddress import IPv4Network
 
 from sqlalchemy.dialects import postgresql
 from sqlalchemy.orm import Mapped, mapped_column
-from wg_wrapper.schemas import WGKeys, WGPeerConfig
 
 from wg_service.models.base import Base
-from wg_service.settings import settings
 
 
 class PeerModel(Base):
@@ -13,4 +11,4 @@ class PeerModel(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     private_key: Mapped[str] = mapped_column()
     public_key: Mapped[str] = mapped_column()
-    allowed_ips: Mapped[IPv4Network] = mapped_column(type_=postgresql.INET)
+    allowed_ips: Mapped[IPv4Network] = mapped_column(type_=postgresql.INET, unique=True)
